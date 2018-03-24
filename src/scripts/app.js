c8adf440b328652e9ef4d7ef2d2defc9e810f69c
Olimjon <osattarov23@gmail.com>
2018-03-24 22:25:37 +0300

    js file

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,6 +1,16 @@
 const slider = require('./common/slider');
 const $ = require('jquery');
+const about = require('./common/about');
+const block = require('./common/block');
+const index = require('./common/index');
+const myworks = require('./common/my-works');
+const welcome = require('./common/welcome');
 
 console.log($);
 
 slider();  //   инициализируем слайдер
+about();  // инициализируем about 
+block();  // инициализируем block 
+index();   // инициализируем index 
+myworks();  // инициализируем myworks 
+welcome();   // инициализируем welcom 
