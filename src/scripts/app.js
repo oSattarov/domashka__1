65547d52aa00e17c6b5172421a48beb6b4fe2019
Olimjon <osattarov23@gmail.com>
2018-03-29 10:57:34 +0300

    done is animat

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -15,37 +15,3 @@ index();   // инициализируем index
 myworks();  // инициализируем myworks 
 welcome();   // инициализируем welcom 
 
-// var parallax = (function (){
-//     var bg = document.querySelector('.hero');
-//     var user = document.querySelector('.user');
-//     var portfolio = document.querySelector('.portfolio__pic');
-
-
-//     return {
-//         move: function(block, windowScroll, strafeAmount) {
-//             var strafe = windowScroll / -strafeAmount + '%';
-
-//             var tr = 'translate3d(0,' + strafe + ',0)';
-
-//             var style = block.style;
-
-//             //style.top = strafe;
-
-//             style.transform = tr;
-//             style.webkitTransform = tr;
-//         },
-
-//         init: function(wScroll){
-//             this.move(bg, wScroll, 45);
-//             this.move(portfolio, wScroll, 20);
-//             this.move(user, wScroll, 3);
-//         }
-//     }
-// }());
-
-
-// window.onscroll = function () {
-//     var wScroll = window.pageYOffset;
-//     console.log(wScroll);
-//     parallax.init(wScroll);
-// }
